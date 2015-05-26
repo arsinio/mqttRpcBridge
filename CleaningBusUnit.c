66146f4e6bf3c21954a333e3ff6dfa0445f169f4
Christopher Armenio <arsinio@gmail.com>
2015-05-25 20:28:05 -0400

    initial commit

---
+++ CleaningBusUnit.c
@@ -0,0 +1,17 @@
+/*
+ * CleaningBusUnit.c
+ *
+ * Created: 5/25/2015 8:25:20 PM
+ *  Author: Christopher Armenio
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
