5df1c6e89cda7cea99b2aeca3d0c21f75527d588
Christopher Armenio <christopher.armenio@opencxa.org>
2015-07-01 18:04:35 -0400

    Squashed 'externals/common-c/' changes from 7b16c47..5fc65f8
    5fc65f8 changed where we set the id of outgoing requests
    4b4aa14 updated usart implementation to be more standardized
    0149181 updated for AVR implementation
    a46379e adjust name length for proper bounds
    
    git-subtree-dir: externals/common-c
    git-subtree-split: 5fc65f823418ec69d79b24f5fb27c555078b0000

--- src/arch-common/cxa_usart.c
+++ src/arch-common/cxa_usart.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 opencxa.org
+ * Copyright 2015 opencxa.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "cxa_xmega_timeBase.h"
+#include "cxa_usart.h"
 
 
 /**
@@ -22,6 +22,7 @@
 
 
 // ******** includes ********
+#include <stdbool.h>
 #include <cxa_assert.h>
 
 
@@ -38,33 +39,12 @@
 
 
 // ******** global function implementations ********
-void cxa_xmega_timeBase_init_timer32(cxa_timeBase_t *const tbIn, cxa_xmega_timer32_t *const timerIn)
+cxa_ioStream_t* cxa_usart_getIoStream(cxa_usart_t* usartIn)
 {
-	cxa_assert(tbIn);
-	cxa_assert(timerIn);
-	
-	// save our references
-	tbIn->timer = timerIn;	
-}
+	cxa_assert(usartIn);
 
-
-uint32_t cxa_timeBase_getCount_us(cxa_timeBase_t *const superIn)
-{
-	cxa_assert(superIn);
-	cxa_timeBase_t *tbIn = (cxa_timeBase_t*)superIn;
-	
-	return cxa_xmega_timer32_getCount(tbIn->timer) * (1000000 / cxa_xmega_timer32_getResolution_cntsPerS(tbIn->timer));
-}
-
-
-uint32_t cxa_timeBase_getMaxCount_us(cxa_timeBase_t *const superIn)
-{
-	cxa_assert(superIn);
-	cxa_timeBase_t *tbIn = (cxa_timeBase_t*)superIn;
-	
-	return cxa_xmega_timer32_getMaxVal_cnts(tbIn->timer) * (1000000 / cxa_xmega_timer32_getResolution_cntsPerS(tbIn->timer));
+	return &usartIn->ioStream;
 }
 
 
 // ******** local function implementations ********
-
